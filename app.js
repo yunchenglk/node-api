2bb45cd5f324f3802bb3d77150f710e677739b05
lk-home <lk-home@163.com>
2017-12-12 00:21:34 +0800

    1

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ app.set('view engine', 'html');
 
 
 app.get('/', function (req, res) {
-    _db.getPage("Class", { index: 1, size: 2 }, { ParentID: "7e146391-25d8-4ea2-94a7-bb0061cf9797" }, function (err, res) {
-        caonsole.log("over");
-        console.log(res);
+    // _db.count("Class", {}, function (err, res) {
+    //     console.log(res);
+    // })
+    _db.getPage("Class", { index: 1, size: 2 }, { ParentID: "7e146391-25d8-4ea2-94a7-bb0061cf9797" }, {}, function (err, data) {
+        // caonsole.log("over");
+        console.log(data);
         res.send("ok");
     })
-    
+
 });
 app.use('/class', auth.isAuthenticated, require('./routes/Class'));
 app.use('/content', auth.isAuthenticated, require('./routes/Content'));
