aa58b8e2ea35ca1d20f716ef74bfc3244842f79a
lk-work <lk-work@163.com>
2017-12-12 20:04:14 +0800

    1

---
+++ services/UserServer.js
@@ -0,0 +1,8 @@
+var _db = require('../lib/_db');
+var logger = require('pomelo-logger').getLogger('mongodb-log');
+module.exports = {
+    login: function (username, password, companyid, callback) {
+
+    }
+
+}
